74bbb9bf892ba5ea2a3e1c28827001ab655c3d47
Kaho Tisthammer <ktisthammer@gmail.com>
2019-11-25 11:26:15 -0800

    R scripts updates and rerun

--- R_scripts/DataFormatting/Connect.R
+++ R_scripts/DataFormatting/Connect.R
@@ -1,95 +1,28 @@
-# your floder where all pages are saved
-#setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
-#cycle thiygh the data folder
+source("R_scripts/DataFormatting/WTAA_consensus.R")
+source("R_scripts/DataFormatting/MUTAA.R")
+source("R_scripts/DataFormatting/Drastic_AA_Change.R")
+source("R_scripts/DataFormatting/CPG_Function.R")
+source("R_scripts/DataFormatting/SynNonSyn.R")
 
-Virus_info<- read.csv("data/list/Final_CpG_list.csv")
-# DataSet <-read.fasta("DengueVirus1.fasta_pruned.mu.trim05.txt")
-for(i in 1:nrow(Virus_info)){
+#Cycle through the data folder
+Virus_info<- list.files("data/", pattern=".fasta")
+for(i in 1:length(Virus_info)){
   print(i)
-  source("R_scripts/Basic_Bioinfo_Rscripts/Freq.R")
+  source("R_scripts/DataFormatting/Freq.R")
   # must place your file as a txt takes a few minutes 
-  #setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-/virus")
-  viruplace = paste(Virus_info$Fasta_File_Path[i],"/fasta/", Virus_info$File[i], sep="")
+  viruplace = paste0("data/",Virus_info[i])
   DF<-Freq(viruplace)
-  name = as.character(Virus_info$File[i])
-  splitname<-unlist(strsplit(as.character(Virus_info$File[i]),".fasta"))
-  truename<-splitname[1]
+  name <- as.character(Virus_info[i])
+  truename<-unlist(strsplit(as.character(Virus_info[i]),".fasta"))
+ 
   DF$wtnt_consensus<-as.character(DF$wtnt_consensus)
   DF$Virus<-(truename)
-  #setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
-  
-  #  
-  # make sure that M is the first amino acid check all virus
-  #no longer needed
-  #start = Virus_info$start[i]
-  #end = Virus_info$stop[i]
-  #DF<-DF[c(start:(nrow(DF)-end)),]
-  #DF$num<-(1:nrow(DF))
-  
-  
-  source("R_scripts/Basic_Bioinfo_Rscripts/WTAA_consensus.R")
+
   DF<-getWTAA(DF)
-  
-  source("R_scripts/Basic_Bioinfo_Rscripts/MUTAA.R")
   DF<-getMUTAA(DF)
-  
-  source("R_scripts/Basic_Bioinfo_Rscripts/Drastic_AA_Change.R")
   DF<-big_aa_change(DF)
-  
-  source("R_scripts/Basic_Bioinfo_Rscripts/CPG_Function.R")
   DF<-CPG_site(DF)
-  
-  source("R_scripts/Basic_Bioinfo_Rscripts/SynNonSyn.R")
   DF<-synFunction(DF)
-  
-  #How to save data
-  #truenameRda= paste('data/Rda/', truename, ".Rda", sep="")
-  #save("virus" = DF,file= truenameRda)
-  truenameCSV= paste(Virus_info$Fasta_File_Path[i],"/Csv/", truename,".csv", sep="")
-  write.csv(DF, file = truenameCSV)
+
+  write.csv(DF, file = paste0("output/Csv/",truename,".csv"))
 }
-
- source("R_scripts/Basic_Bioinfo_Rscripts/SynNonSyn.R")
-DF<-synFunction(DF)
-
-source("R_scripts/Basic_Bioinfo_Rscripts/CPG_Function.R")
-DF<-CPG_site(DF)
-#How to load data
-virusname = 'EnterovirusB_VP2.fasta_pruned.mu.trim05.txt'
-splitname<-unlist(strsplit(virusname,".fasta"))
-truename<-splitname[1]
-
-#truenameRda= paste('data/Rda/', truename, ".Rda", sep="")
-truenameCSV= paste('data/output/Csv/', truename, ".csv", sep="")
-#write.csv(DF, file = "MyData.csv")
-
-#setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-/Rda_Files")
-# load(truenameRda)
-###############################################
-# graphs/ tables 
-#setwd("~/Desktop/Git/CpG/Something_Cool-CpG_Sites-")
-# source("R_scripts/BioInfo/Synonymous and nonsynonymous graph.R")
-# SynNonsynAT(DF)
-# SynNonsynCG(DF)
-# 
-# 
-# source("R_scripts/BioInfo/MakesCPG Graph.R")
-# CPGNoCPGAT(DF)
-# 
-# source("R_scripts/BioInfo/CPG_Syn_Nonsyn_graph.R")
-# comparing_CpG_Syn_Nonsyn (DF)
-
-# Wilcox test
-#source("stats.R")
-#Wilcox_test(DF)
-# that tables
-#source("R_scripts/BioInfo/RyanWilcox.R")
-#Wilcox_test(DF, truename)
-
-#source("R_scripts/graphs/redoplot.R")
-#comparing_CpG_Syn_Nonsyn_new(DF, truename)
-
-#########
-########
-
-
